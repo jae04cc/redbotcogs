1029a0501904f02644d468c62efda23cb6e41c69
jae04cc <jae04cc@gmail.com>
2021-12-17 13:26:48 -0500

    fixes

--- threadmanager/threadmanager.py
+++ threadmanager/threadmanager.py
@@ -14,11 +14,7 @@ async def create_thread(bot, channel: discord.TextChannel, message: discord.Mess
         int: The channel ID of the newly created thread
     """
     guild = channel.guild
-    if archive > 4320 and "THREE_DAY_THREAD_ARCHIVE" not in guild.features:
-        archive = 1440
-    elif archive == 10080 and "SEVEN_DAY_THREAD_ARCHIVE" not in guild.features:
-        archive = 4320
-
+    archive = 4320
     fields = {"name": name, "auto_archive_duration": archive}
     reason = "Thread Manager"
 
@@ -47,11 +43,35 @@ class threadmanager(commands.Cog):
 
     async def thread(self, ctx, *, name: str):
         """
-        Create a new thread from this channel
-        You must have proper permissions set
+        Create a new thread
         """
         channel = ctx.channel
         guild = ctx.guild
         user = ctx.author
 
         await create_thread(self.bot, channel, ctx.message, name=name, archive=archive)
+
+    async def mkthread(bot, channel: discord.TextChannel, message: discord.Message, name: str, archive: int = 4320):
+        """
+        Creates a new thread in the channel from the message
+        Args:
+            channel (TextChannel): The channel the thread will be apart of
+            message (Message): The discord message the thread will start with
+            name (str): The name of the thread
+            archive (int): The archive duration. Can be 60, 1440, 4320, and 10080.
+        Returns:
+            int: The channel ID of the newly created thread
+        """
+        guild = channel.guild
+        archive = 4320
+        fields = {"name": name, "auto_archive_duration": archive}
+        reason = "Thread Manager"
+
+        r = Route(
+            "POST",
+            "/channels/{channel_id}/messages/{message_id}/threads",
+            channel_id=channel.id,
+            message_id=message.id,
+        )
+
+        return (await bot.http.request(r, json=fields, reason=reason))["id"]

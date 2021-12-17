75c36ee877a0a093720f19ce230c1ac98ee51006
jae04cc <jae04cc@gmail.com>
2021-12-17 12:52:48 -0500

    fixes

--- threadmanager/threadmanager.py
+++ threadmanager/threadmanager.py
@@ -1,26 +1,43 @@
 import discord
+from discord.http import Route
 from redbot.core import Config, checks, commands
 
-async def create_thread(self,name,minutes,message):
-    token = 'Bot ' + self._state.http.token
-    url = f"https://discord.com/api/v9/channels/{self.id}/messages/{message.id}/threads"
-    headers = {
-        "authorization" : token,
-        "content-type" : "application/json"
-    }
-    data = {
-        "name" : name,
-        "type" : 11,
-        "auto_archive_duration" : minutes
-    }
- 
-    return requests.post(url,headers=headers,json=data).json()
+async def create_thread(bot, channel: discord.TextChannel, message: discord.Message, name: str, archive: int = 4320):
+    """
+    Creates a new thread in the channel from the message
+    Args:
+        channel (TextChannel): The channel the thread will be apart of
+        message (Message): The discord message the thread will start with
+        name (str): The name of the thread
+        archive (int): The archive duration. Can be 60, 1440, 4320, and 10080.
+    Returns:
+        int: The channel ID of the newly created thread
+    """
+    guild = channel.guild
+    if archive > 4320 and "THREE_DAY_THREAD_ARCHIVE" not in guild.features:
+        archive = 1440
+    elif archive == 10080 and "SEVEN_DAY_THREAD_ARCHIVE" not in guild.features:
+        archive = 4320
 
-discord.TextChannel.create_thread = create_thread
-Bot = discord.Client()
- 
-@Bot.event
-async def on_message(ctx):
-    if ctx.content == "?thread":
-        f = await ctx.channel.create_thread(name="Thread", minutes=4320, message=ctx)
-Bot.run("token")
+    fields = {"name": name, "auto_archive_duration": archive}
+    reason = "Thread Manager"
+
+    r = Route(
+        "POST",
+        "/channels/{channel_id}/messages/{message_id}/threads",
+        channel_id=channel.id,
+        message_id=message.id,
+    )
+
+    return (await bot.http.request(r, json=fields, reason=reason))["id"]
+
+async def thread(self, ctx, *, name: str):
+    """
+    Create a new thread from this channel
+    You must have proper permissions set
+    """
+    channel = ctx.channel
+    guild = ctx.guild
+    user = ctx.author
+    
+    await create_thread(self.bot, channel, ctx.message, name=name, archive=archive)
